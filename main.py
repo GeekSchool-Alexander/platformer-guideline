e21e0befd1cfb4b4053c3773b4f08166441ec5cf
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2018-07-08 23:51:25 +0300

    Скелет игры используя ООП

---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame as pg  # Подключение pygame с именем pg
+from settings import *  # Подключение файла с настройками
+
+
+class Game:
+	def __init__(self):  # Конструктор
+		pg.init()  # Запуск pygame
+		self.screen = pg.display.set_mode(WINDOW_SIZE)  # Создание окна с размером из настроек
+		pg.display.set_caption("GeekSchool Platformer")  # Заголовок окна
+		self.clock = pg.time.Clock()  # Объект для отслеживания времени (тактов)
+		self.running = True  # Флаг продолжения работы программы
+	
+	def new(self):  # Создание нового уровня
+		self.all_sprites = pg.sprite.Group()  # Создание группы для всех спрайтов
+		self.run()  # Запускаем уровень
+	
+	def events(self):  # Цикл обработки событий
+		for event in pg.event.get():  # Берем события на текущем такте
+			if event.type == pg.QUIT:  # Если пришло событие о выходе (нажатие на крестик или Alt+F4)
+				self.playing = False  # Останавливаем уровень
+				self.running = False  # Останавливаем основной цикл программы
+	
+	def update(self):  # Обновление состояния игровых объектов
+		self.all_sprites.update()  # Обновление всех спрайтов
+	
+	def draw(self):  # Отрисовка
+		self.screen.fill(WHITE)  # Заливка окна белым цветом
+		self.all_sprites.draw(self.screen)  # Отрисовка всех спрайтов
+		pg.display.flip()  # Отображение кадра из буфера на экран
+	
+	def run(self):  # Основной цикл
+		self.playing = True  # Флаг продолжения уровня
+		while self.playing:  # Если продолжать уровень
+			self.clock.tick(FPS)  # Работа на частоте из настроек
+			self.events()  # Обработка событий
+			self.update()  # Обновление игровых объектов
+			self.draw()  # Отрисовка
+	
+	def main(self):  # Основная функция
+		while self.running:  # Если продолжать программу
+			self.new()  # Создать уровень
+	
+	def __del__(self):  # Деструктор
+		pg.quit()  # Завершение работы pygame
+
+
+g = Game()  # Создание игры
+g.main()  # Вызов главной функции
