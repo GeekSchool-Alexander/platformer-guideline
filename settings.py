c6401265b614e1425a8b0633760d80c67a6454c0
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2018-07-08 23:51:04 +0300

    Файл с настройками

---
+++ settings.py
@@ -0,0 +1,6 @@
+# Файл с настройками
+WHITE = (255, 255, 255)  # Белый цвет для фона
+WINDOW_WIDTH = 1000  # Ширина окна
+WINDOW_HEIGHT = 600  # Высота окна
+WINDOW_SIZE = (WINDOW_WIDTH, WINDOW_HEIGHT)  # Кортеж размеров окна
+FPS = 60  # Frames per Second; Частота, на которой наботает основной цикл
